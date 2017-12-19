b5c88810aeecdbe058b5379723e3bb7e95d20247
brageanth <brageanth@gmail.com>
2017-12-19 14:14:21 -0500

    Cree todo el funcionamiento del modulo 'He olvidado la contraseña'

--- Noticiero/settings.py
+++ Noticiero/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'info@paramopresenta.com'
+EMAIL_HOST_PASSWORD = 'bajofondo2004'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- Noticiero/urls.py
+++ Noticiero/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.auth.views import password_reset, password_reset_done,\
+    password_reset_confirm, password_reset_complete
 
 urlpatterns = [
         url(r'^admin/', include(admin.site.urls)),
@@ -10,5 +12,9 @@ urlpatterns = [
         }),
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT
-        }), 
+        }),
+        url(r'^reset/password_reset$', password_reset, {'template_name':'Reset/password_reset.html', 'email_template_name':'Reset/password_reset_email.html'}, name='password_reset'),
+        url(r'^reset/password_reset_done$', password_reset_done, {'template_name':'Reset/password_reset_done.html'}, name='password_reset_done'),
+        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, {'template_name':'Reset/password_reset_confirm.html'}, name='password_reset_confirm'),
+        url(r'^reset/password_reset_complete', password_reset_complete, {'template_name':'Reset/password_final.html'}, name='password_reset_complete'), 
 ]

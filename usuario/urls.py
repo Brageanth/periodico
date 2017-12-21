4e4da31b6e616ba72306a5518de99ca389f0f034
Julian Rique <JulianRique@MacBook-Pro-de-mac02.local>
2017-12-21 16:41:55 -0500

    Url Creada

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from usuario.views import RegistroUsuario
+from django.contrib.auth import logout
 
 urlpatterns = [
     url(r'^$', RegistroUsuario.as_view(), name="registrar"),
+    url(r'salir$', logout, name="salira", kwargs={'next_page': '/'}),
 ]
